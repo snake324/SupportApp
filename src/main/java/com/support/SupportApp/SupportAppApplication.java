eba5b0156143df64b4829cc562082519745ec00c
snake324 <kristi4n90@gmail.com>
2023-07-10 09:23:48 +0200

    springboot

---
+++ src/main/java/com/support/SupportApp/SupportAppApplication.java
@@ -0,0 +1,13 @@
+package com.support.SupportApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SupportAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SupportAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/support/SupportApp/SupportAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.support.SupportApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SupportAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
